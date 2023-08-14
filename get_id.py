919ad3a6e6b102e875f7c6d148bcf937a8ee1a9c
Connie-Wild <34596022+Connie-Wild@users.noreply.github.com>
2023-08-14 20:28:20 +0900

    update

--- get_id.py
+++ get_id.py
@@ -8,8 +8,10 @@ s = requests.Session()
 
 url = 'https://www.fortiguard.com/appcontrol'
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
-headers = {'User-Agent': user_agent, 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'}
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+headers = {
+    'User-Agent': user_agent
+}
 
 while True:
     try:
